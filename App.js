c6ca42eb17eead729e7cb0e9cc938df98070fe8d
Henry Hartzler <hartzler.henry@gmail.com>
2022-11-12 14:49:53 -0700

    created all initial screens

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
 import Main from './screens/MainComponent';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <Provider>
-
-    </Provider>
+    <NavigationContainer>
+      <Main />
+    </NavigationContainer>
+    
   );
 }
 
