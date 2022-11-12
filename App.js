4bf868d9afd06e0d5532d7224868fba0cd2b7fac
Henry Hartzler <hartzler.henry@gmail.com>
2022-11-12 14:06:03 -0700

    imported redux dependencies

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Main from './screens/MainComponent';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Whoa</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+
+    </Provider>
   );
 }
 
---
+++ screens/MainComponent.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Main = () => {
+    return (
+        <View>
+            <Text>Navigators will go here</Text>
+        </View>
+      );
+}
+ 
+export default Main;
