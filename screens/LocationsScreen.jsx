a3d6777fb40e838e120d56bf99d96f4c5ee2bbb8
Henry Hartzler <hartzler.henry@gmail.com>
2022-11-13 16:53:14 -0700

    titles and search bar

--- screens/LocationsScreen.jsx
+++ screens/LocationsScreen.jsx
@@ -1,11 +1,55 @@
-import { View, Text } from 'react-native';
+import {
+    StyleSheet, 
+    View, 
+    Text
+} from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 const LocationsScreen = () => {
     return (
-        <View>
-            <Text>LocationsScreen will go here</Text>
+        <View style={styles.screen}>
+            <View style={styles.header}>
+                <Text style={styles.headerSubtitle}>
+                    Hi, Henry
+                </Text>
+                <Text style={styles.headerTitle}>
+                    WHERE ARE YOU CLIMBING TODAY?
+                </Text>
+            </View>
+            <View style={{ padding: 10 }}>
+                <SearchBar 
+                    round
+                    
+                    placeholder='Climbs, locations, and people'
+                />
+            </View>
+            
         </View>
       );
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        backgroundColor: '#000',
+        flex: 1,
+        padding: 10,
+        justifyContent: 'flex-start'
+    },
+    header: {
+        marginTop: 10,
+        marginLeft: 10
+    },
+    headerSubtitle: {
+        color:'#fff',
+        paddingBottom: 5,
+        fontSize: 22
+    },
+    headerTitle: {
+        color: '#FFFF00',
+        paddingRight: 50,
+        fontSize: 32,
+        fontWeight: 'bold'    
+    }
+})
  
 export default LocationsScreen;
