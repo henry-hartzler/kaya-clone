556604fcef643ff6d3a9f87a185180908fd236be
Henry Hartzler <hartzler.henry@gmail.com>
2022-11-12 17:32:52 -0700

    navigation is complete

---
+++ screens/ForYouScreen.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const ForYouScreen = () => {
+    return (
+        <View>
+            <Text>ForYouScreen will go here</Text>
+        </View>
+      );
+}
+ 
+export default ForYouScreen;
--- screens/MainComponent.jsx
+++ screens/MainComponent.jsx
@@ -1,5 +1,14 @@
 import { StyleSheet, View, Text, Platform } from "react-native";
 import Constants from 'expo-constants';
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Icon } from 'react-native-elements';
+import ForYouScreen from "./ForYouScreen";
+import AchievementsScreen from "./AchievementsScreen";
+import LocationsScreen from "./LocationsScreen";
+import ProfileScreen from "./ProfileScreen";
+
+const Tab = createBottomTabNavigator();
 
 const Main = () => {
     return (
@@ -8,7 +17,82 @@ const Main = () => {
                 flex: 1,
                 paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
             }}>
-                <Text>Navigators will go here</Text>
+                <Tab.Navigator 
+                    screenOptions={{
+                        tabBarStyle: {
+                            backgroundColor: '#000',
+                            color: '#fff'
+                        },
+                        tabBarActiveTintColor: '#fff',
+                        tabBarShowLabel: false
+                    }}
+                    initialRouteName="For You"
+                >
+                    <Tab.Screen 
+                        name="For You" 
+                        component={ForYouScreen}
+                        options={{ 
+                            title: 'For You',
+                            tabBarIcon: ({ color }) => (
+                                <Icon 
+                                    name="play"
+                                    type="font-awesome"
+                                    size={24}
+                                    iconStyle={{ width: 24 }}
+                                    color={color}
+                                />
+                            ) 
+                        }} 
+                    />
+                    <Tab.Screen 
+                        name="Achievements" 
+                        component={AchievementsScreen}
+                        options={{ 
+                            title: 'Achievements',
+                            tabBarIcon: ({ color }) => (
+                                <Icon 
+                                    name="trophy"
+                                    type="font-awesome"
+                                    size={24}
+                                    iconStyle={{ width: 24 }}
+                                    color={color}
+                                />
+                            ) 
+                        }}  
+                    />
+                    <Tab.Screen 
+                        name="Locations" 
+                        component={LocationsScreen}
+                        options={{ 
+                            title: 'Locations',
+                            tabBarIcon: ({ color }) => (
+                                <Icon 
+                                    name="map"
+                                    type="font-awesome"
+                                    size={24}
+                                    iconStyle={{ width: 24 }}
+                                    color={color}
+                                />
+                            ) 
+                        }}  
+                    />
+                    <Tab.Screen 
+                        name="Profile" 
+                        component={ProfileScreen}
+                        options={{ 
+                            title: 'Profile',
+                            tabBarIcon: ({ color }) => (
+                                <Icon 
+                                    name="user"
+                                    type="font-awesome"
+                                    size={24}
+                                    iconStyle={{ width: 24 }}
+                                    color={color}
+                                />
+                            ) 
+                        }}  
+                    />
+                </Tab.Navigator>
             </View>
         </>
       );
@@ -18,7 +102,7 @@ const styles = StyleSheet.create({
     mainFooter: {
         backgroundColor: 'black',
         color: '#fff',
-        padding: 20
+        padding: 100
     }
 })
  
