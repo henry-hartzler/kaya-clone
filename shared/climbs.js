8ab88592fef1ff53e0f4fc005c9d478bbf686623
Henry Hartzler <hartzler.henry@gmail.com>
2022-11-13 15:55:30 -0700

    hard coded data placeholder for climbs.js

---
+++ shared/climbs.js
@@ -0,0 +1,48 @@
+import V7img from '../assets/V7.png';
+import V8img from '../assets/V8.png';
+import V9img from '../assets/V9.png';
+
+export const CLIMBS = [
+    {
+        id: 0,
+        name: "Burly",
+        grade: 'V7',
+        image: V7img,
+        location: 'Little Cottonwood Canyon'
+    },
+    {
+        id: 1,
+        name: "Bear Hug",
+        grade: 'V8',
+        image: V8img,
+        location: 'Little Cottonwood Canyon'
+    },
+    {
+        id: 2,
+        name: "Fat Grips",
+        grade: 'V9',
+        image: V9img,
+        location: 'Little Cottonwood Canyon'
+    },
+    {
+        id: 3,
+        name: "Pandemic",
+        grade: 'V7',
+        image: V7img,
+        location: 'Joes Valley'
+    },
+    {
+        id: 4,
+        name: "Dead Sea",
+        grade: 'V8',
+        image: V8img,
+        location: 'Joes Valley'
+    },
+    {
+        id: 5,
+        name: "Worst Case Scenario",
+        grade: 'V9',
+        image: V9img,
+        location: 'Joes Valley'
+    },
+]
