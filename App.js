e9637b6de682ed3ccdd8ec5f34a815066e9b8880
Henry Hartzler <hartzler.henry@gmail.com>
2022-11-13 16:21:15 -0700

    added redux store

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import Main from './screens/MainComponent';
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Main />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Main />
+      </NavigationContainer>
+    </Provider>
+    
     
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
