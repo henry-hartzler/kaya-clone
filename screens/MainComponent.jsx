658f31514f9768da41b1f3139afa45842c0578d6
Henry Hartzler <hartzler.henry@gmail.com>
2022-11-23 14:32:31 -0700

    Main location screen uses flatlist

--- screens/MainComponent.jsx
+++ screens/MainComponent.jsx
@@ -1,144 +1,158 @@
-import { StyleSheet, View, Text, Platform } from "react-native";
-import Constants from 'expo-constants';
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Icon } from 'react-native-elements';
-import ForYouScreen from "./ForYouScreen";
-import AchievementsScreen from "./AchievementsScreen";
-import LocationsScreen from "./LocationsScreen";
-import ProfileScreen from "./ProfileScreen";
-import SearchScreen from "./SearchScreen";
-import { getFocusedRouteNameFromRoute, NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, View, Text, Platform } from "react-native"
+import Constants from "expo-constants"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Icon } from "react-native-elements"
+import ForYouScreen from "./ForYouScreen"
+import AchievementsScreen from "./AchievementsScreen"
+import LocationsScreen from "./LocationsScreen"
+import ProfileScreen from "./ProfileScreen"
+import SearchScreen from "./SearchScreen"
+import LocationInfoScreen from "./LocationInfoScreen"
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native"
 
 function getHeaderTitle(route) {
-    const routeName = getFocusedRouteNameFromRoute(route) ?? 'For You'
+	const routeName = getFocusedRouteNameFromRoute(route) ?? "For You"
 
-    switch(routeName) {
-        case 'For You':
-            return 'For You';
-        case 'Achievements':
-            return 'Achievements';
-        case 'Locations':
-            return 'Locations';
-        case 'Profile': 
-            return 'Profile';
-        case 'Search': 
-            return 'Search';
-    }
+	switch (routeName) {
+		case "For You":
+			return "For You"
+		case "Achievements":
+			return "Achievements"
+		case "Locations":
+			return "Locations"
+		case "Profile":
+			return "Profile"
+		case "Search":
+			return "Search"
+	}
 }
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 
 const HomeTabs = () => {
-    return (
-                <Tab.Navigator 
-                    screenOptions={{
-                        tabBarStyle: {
-                            backgroundColor: '#000',
-                            color: '#fff'
-                        },
-                        tabBarActiveTintColor: '#fff',
-                        tabBarShowLabel: false,
-                        headerShown: false
-                    }}
-                    initialRouteName="For You"
-                >
-                    <Tab.Screen 
-                        name="For You" 
-                        component={ForYouScreen}
-                        options={{ 
-                            title: 'For You',
-                            tabBarIcon: ({ color }) => (
-                                <Icon 
-                                    name="play"
-                                    type="font-awesome"
-                                    size={24}
-                                    iconStyle={{ width: 24 }}
-                                    color={color}
-                                />
-                            ) 
-                        }} 
-                    />
-                    <Tab.Screen 
-                        name="Achievements" 
-                        component={AchievementsScreen}
-                        options={{ 
-                            title: 'Achievements',
-                            tabBarIcon: ({ color }) => (
-                                <Icon 
-                                    name="trophy"
-                                    type="font-awesome"
-                                    size={24}
-                                    iconStyle={{ width: 24 }}
-                                    color={color}
-                                />
-                            ) 
-                        }}  
-                    />
-                    <Tab.Screen 
-                        name="Locations" 
-                        component={LocationsScreen}
-                        options={{ 
-                            title: 'Locations',
-                            tabBarIcon: ({ color }) => (
-                                <Icon 
-                                    name="map"
-                                    type="font-awesome"
-                                    size={24}
-                                    iconStyle={{ width: 24 }}
-                                    color={color}
-                                />
-                            ) 
-                        }}  
-                    />
-                    <Tab.Screen 
-                        name="Profile" 
-                        component={ProfileScreen}
-                        options={{ 
-                            title: 'Profile',
-                            tabBarIcon: ({ color }) => (
-                                <Icon 
-                                    name="user"
-                                    type="font-awesome"
-                                    size={24}
-                                    iconStyle={{ width: 24 }}
-                                    color={color}
-                                />
-                            ) 
-                        }}  
-                    />
-                </Tab.Navigator>
-            
-      );
+	return (
+		<Tab.Navigator
+			screenOptions={{
+				tabBarStyle: {
+					backgroundColor: "#000",
+					color: "#fff",
+				},
+				tabBarActiveTintColor: "#fff",
+				tabBarShowLabel: false,
+				headerShown: false,
+			}}
+			initialRouteName='For You'
+		>
+			<Tab.Screen
+				name='For You'
+				component={ForYouScreen}
+				options={{
+					title: "For You",
+					tabBarIcon: ({ color }) => (
+						<Icon
+							name='play'
+							type='font-awesome'
+							size={24}
+							iconStyle={{ width: 24 }}
+							color={color}
+						/>
+					),
+				}}
+			/>
+			<Tab.Screen
+				name='Achievements'
+				component={AchievementsScreen}
+				options={{
+					title: "Achievements",
+					tabBarIcon: ({ color }) => (
+						<Icon
+							name='trophy'
+							type='font-awesome'
+							size={24}
+							iconStyle={{ width: 24 }}
+							color={color}
+						/>
+					),
+				}}
+			/>
+			<Tab.Screen
+				name='Locations'
+				component={LocationsScreen}
+				options={{
+					title: "Locations",
+					tabBarIcon: ({ color }) => (
+						<Icon
+							name='map'
+							type='font-awesome'
+							size={24}
+							iconStyle={{ width: 24 }}
+							color={color}
+						/>
+					),
+				}}
+			/>
+			<Tab.Screen
+				name='Profile'
+				component={ProfileScreen}
+				options={{
+					title: "Profile",
+					tabBarIcon: ({ color }) => (
+						<Icon
+							name='user'
+							type='font-awesome'
+							size={24}
+							iconStyle={{ width: 24 }}
+							color={color}
+						/>
+					),
+				}}
+			/>
+		</Tab.Navigator>
+	)
 }
 
 const Stack = createNativeStackNavigator()
 
 const Main = () => {
-    return (
-        <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight }}>
-            <Stack.Navigator
-                screenOptions={{
-                    headerShown: false
-                }}
-            >
-               <Stack.Screen
-                   name="Home"
-                   component={HomeTabs}
-               />
-                <Stack.Screen name="Search" component={SearchScreen} />
-            </Stack.Navigator>
-        </View>
-    )
+	return (
+		<View
+			style={{
+				flex: 1,
+				paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
+			}}
+		>
+			<Stack.Navigator
+				screenOptions={{
+					headerShown: false,
+				}}
+			>
+				<Stack.Screen
+					name='Home'
+					component={HomeTabs}
+				/>
+				<Stack.Screen
+					name='Search'
+					component={SearchScreen}
+				/>
+				<Stack.Screen
+					name='LocationInfo'
+					component={LocationInfoScreen}
+					options={({ route }) => ({
+						title: route.params.location.name,
+					})}
+				/>
+			</Stack.Navigator>
+		</View>
+	)
 }
-        
 
 const styles = StyleSheet.create({
-    mainFooter: {
-        backgroundColor: 'black',
-        color: '#fff',
-        padding: 100
-    }
+	mainFooter: {
+		backgroundColor: "black",
+		color: "#fff",
+		padding: 100,
+	},
 })
- 
-export default Main;
+
+export default Main
--- screens/SearchScreen.jsx
+++ screens/SearchScreen.jsx
@@ -1,96 +1,129 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { SearchBar, Avatar, ListItem } from 'react-native-elements';
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { Text, View, StyleSheet } from "react-native"
+import { SearchBar, Avatar, ListItem } from "react-native-elements"
+import { useState } from "react"
+import { useSelector } from "react-redux"
 
 const SearchScreen = ({ navigation }) => {
-    const [search, setSearch] = useState('');
-    const updateSearch = search => {
-        setSearch(search)
-        setFilteredLocations(locations.filter(el => el.name.toLowerCase().includes(search.toLowerCase())))
-        setFilteredClimbs(climbs.filter(el => el.name.toLowerCase().includes(search.toLowerCase())))
-    }
+	const [search, setSearch] = useState("")
+	const updateSearch = (search) => {
+		setSearch(search)
+		setFilteredLocations(
+			locations.filter((el) =>
+				el.name.toLowerCase().includes(search.toLowerCase())
+			)
+		)
+		setFilteredClimbs(
+			climbs.filter((el) =>
+				el.name.toLowerCase().includes(search.toLowerCase())
+			)
+		)
+	}
 
-    const locations = useSelector(state => state.locations.locationsArray)
-    const climbs = useSelector(state => state.climbs.climbsArray)
-    
-    const [filteredLocations, setFilteredLocations] = useState(
-        locations.filter(el => el.name.includes('')))
-    const [filteredClimbs, setFilteredClimbs] = useState(
-        climbs.filter(el => el.name.includes('')))
+	const locations = useSelector((state) => state.locations.locationsArray)
+	const climbs = useSelector((state) => state.climbs.climbsArray)
 
-    return (
-        <View>
-            <SearchBar 
-                placeholder='Type here'
-                onChangeText={updateSearch}
-                value={search}
-            />
-            <Text>Locations</Text>
-            {filteredLocations.map(location => (
-                <ListItem 
-                    style={styles.listItem} 
-                    key={location.id}
-                    onPress={() => {
-                        navigation.navigate('Home')
-                    }}
-                >
-                    <Avatar rounded source={location.image} />
-                    <ListItem.Content style={styles.listItemContent}>
-                        <ListItem.Title>{location.name}</ListItem.Title>
-                        <ListItem.Subtitle>{location.state}</ListItem.Subtitle>
-                    </ListItem.Content>
-                </ListItem>
-            ))}
-            <Text>Climbs</Text>
-            {filteredClimbs.map(climb => (
-                <ListItem 
-                    style={styles.listItem} 
-                    key={climb.id}
-                >
-                    <Avatar rounded source={climb.image} />
-                    <ListItem.Content style={styles.listItemContent}>
-                        <ListItem.Title>{climb.name}</ListItem.Title>
-                        <ListItem.Subtitle>{climb.grade}</ListItem.Subtitle>
-                    </ListItem.Content>
-                </ListItem>
-            ))}
-        </View>
-        
-    );
+	const [filteredLocations, setFilteredLocations] = useState(
+		locations.filter((el) => el.name.includes(""))
+	)
+	const [filteredClimbs, setFilteredClimbs] = useState(
+		climbs.filter((el) => el.name.includes(""))
+	)
+
+	const LocationsMap = () => {
+		return filteredLocations.map((location) => (
+			<ListItem
+				style={styles.listItem}
+				key={location.id}
+				onPress={() => {
+					navigation.navigate("Home")
+				}}
+			>
+				<Avatar
+					rounded
+					source={location.image}
+				/>
+				<ListItem.Content style={styles.listItemContent}>
+					<ListItem.Title>{location.name}</ListItem.Title>
+					<ListItem.Subtitle>{location.state}</ListItem.Subtitle>
+				</ListItem.Content>
+			</ListItem>
+		))
+	}
+
+	const ClimbsMap = () => {
+		return filteredClimbs.map((climb) => (
+			<ListItem
+				style={styles.listItem}
+				key={climb.id}
+			>
+				<Avatar
+					rounded
+					source={climb.image}
+				/>
+				<ListItem.Content style={styles.listItemContent}>
+					<ListItem.Title>{climb.name}</ListItem.Title>
+					<ListItem.Subtitle>{climb.grade}</ListItem.Subtitle>
+				</ListItem.Content>
+			</ListItem>
+		))
+	}
+
+	return (
+		<View>
+			<SearchBar
+				placeholder='Type here'
+				onChangeText={updateSearch}
+				value={search}
+			/>
+
+			{search && filteredLocations.length !== 0 ? (
+				<>
+					<Text>Locations</Text>
+					<LocationsMap />
+				</>
+			) : (
+				<Text>No locations yet</Text>
+			)}
+			{search && filteredClimbs.length !== 0 ? (
+				<>
+					<Text>Climbs</Text>
+					<ClimbsMap />
+				</>
+			) : (
+				<Text>No climbs yet</Text>
+			)}
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        backgroundColor: '#000',
-        flex: 1,
-        padding: 10,
-        justifyContent: 'flex-start'
-    },
-    header: {
-        marginTop: 10,
-        marginLeft: 10
-    },
-    headerSubtitle: {
-        color:'#fff',
-        paddingBottom: 5,
-        fontSize: 22
-    },
-    headerTitle: {
-        color: '#FFFF00',
-        paddingRight: 50,
-        fontSize: 32,
-        fontWeight: 'bold'    
-    },
-    listItem: {
-        marginVertical: 5,
-        backgroundColor: '#000',
-        color: '#fff'
-    },
-    listItemContent: {
-        
-    }
-
+	screen: {
+		backgroundColor: "#000",
+		flex: 1,
+		padding: 10,
+		justifyContent: "flex-start",
+	},
+	header: {
+		marginTop: 10,
+		marginLeft: 10,
+	},
+	headerSubtitle: {
+		color: "#fff",
+		paddingBottom: 5,
+		fontSize: 22,
+	},
+	headerTitle: {
+		color: "#FFFF00",
+		paddingRight: 50,
+		fontSize: 32,
+		fontWeight: "bold",
+	},
+	listItem: {
+		marginVertical: 5,
+		backgroundColor: "#000",
+		color: "#fff",
+	},
+	listItemContent: {},
 })
- 
-export default SearchScreen;
+
+export default SearchScreen
