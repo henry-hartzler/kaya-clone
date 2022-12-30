151f1eb80bd7af0b0158f9153ff13c52eb525fb2
Henry Hartzler <hartzler.henry@gmail.com>
2022-12-29 19:11:16 -0700

    connected to mongodb

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const { MongoClient } = require('mongodb')
+let db
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+	const allClimbs = await db.collection('climbs').find().toArray()
+	console.log(allClimbs)
 	res.send('Hello World!')
 })
 
-app.listen(8000)
+const start = async () => {
+	const client = new MongoClient('mongodb://localhost:27017/kaya-clone')
+	await client.connect()
+	db = client.db()
+	app.listen(8000)
+}
+start()
